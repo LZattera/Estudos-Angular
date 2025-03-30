fbfd1fb8d73763d5280aeeeee790c9709097fdcc
Letícia <leticiazattera949@gmail.com>
2025-03-30 11:27:58 -0300

    primeiro video do projeto
    Curso criando uma aplicação angular tailwind e .net usando dapper

---
+++ ProjetoAngularDapperTailwind/src/app/pages/home/home.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioListar } from '../../models/Usuario';
+import { UsuarioService } from '../../services/usuario.service';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+
+  usuarios: UsuarioListar[] = [];
+  usuariosGeral: UsuarioListar[] = [];
+
+  constructor(
+    private serviceUsuario :UsuarioService
+  ){}
+
+  ngOnInit(): void {
+    this.serviceUsuario.GetUsuarios().subscribe({
+      next:(res)=>{
+        this.usuarios = res.dados;
+        this.usuariosGeral = res.dados;
+        console.log(res.dados)
+      }
+    })  
+  }
+
+  search(event:Event){
+    const target = event.target as HTMLInputElement;
+    const value = target.value.toLowerCase();
+
+    this.usuarios = this.usuariosGeral.filter(usuario =>{
+      return usuario.nomeCompleto.toLowerCase().includes(value);
+    });
+  }
+
+  delete(idUsuario:number|undefined){
+    console.log("Excluir")
+    this.serviceUsuario.DeleteUsuario(idUsuario).subscribe({
+      next:(res)=>{
+       window.location.reload();//recarrega a pagina
+      },
+      error(err) {
+          //colocar um popover onde informa a mensagem de erro ao deletar um usuario res.mensagem
+      },
+    });
+  }
+
+}
